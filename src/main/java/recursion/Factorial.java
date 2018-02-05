3882e5a10bc1a6725e8545b20e46eac7c224edac
dikshit.rajkhowa <dikshit.rajkhowa@IND_DIKSHITRAJKHOWA.in.os.local>
2018-02-06 02:40:10 +0530

    FractalTree

--- src/main/java/recursion/Factorial.java
+++ src/main/java/recursion/Factorial.java
@@ -3,7 +3,7 @@ package recursion;
 public class Factorial {
 
 	public static void main(String[] args) {
-		System.out.println("" + factorialIllustrated(5));
+		System.out.println("\nFactorial :: " + factorialIllustrated(5));
 
 	}
 
@@ -23,14 +23,15 @@ public class Factorial {
 		if (n < 0)
 			throw new IllegalArgumentException("Please enter a positive number");
 		if (n == 0) {
-			System.out.println("Base Case = 1");
+			System.out.println("\nReached Base Case = 1\n");
 			return 1;
 		}
 
 		else {
+			System.out.println("Calling " + n + " * factorial(" + (n - 1)+ ")");
 			int x = factorialIllustrated(n-1);
 			
-			System.out.println("Computing " + n + " * factorial(" + (n - 1)+ ") => "+n+" * "+x);
+			System.out.println("Computing " + n + " * factorial(" + (n - 1)+ ") => "+n+" * "+x +" = "+(n*x));
 			return n * x;
 		}
 
